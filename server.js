6711165773ca3164928f0089494da75d63d64133
Sarfaraz Khan <75362218+sarfaraz-1786@users.noreply.github.com>
2022-03-04 12:04:49 +0530

    Revert "some usefull comments"

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require("dotenv").config({path:"./config.env"}) //always on top so that below can access process.env
+require("dotenv").config({path:"./config.env"})
 const express  = require('express')
 
 const app = express()
 
-app.use(express.json()) // so that we get res.json in routes controllers
+app.use(express.json())
 
 app.use('/api/auth', require('./routes/AuthRoutes'))
 
